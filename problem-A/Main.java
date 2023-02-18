83a91c8a0b64d199e9d95cd6e5c1bbaa1bf10d57
AyauIskakova <120235865+AyauIskakova@users.noreply.github.com>
2023-02-18 23:31:10 +0600

    Update Main.java

--- problem-A/Main.java
+++ problem-A/Main.java
@@ -3,19 +3,19 @@ public class Main {
     public static void main(String[] args) throws NumberFormatException{ 
         Scanner scan  = new Scanner(System.in); 
         try{ 
-            int c = scan.nextInt(); 
-            int d = scan.nextInt(); 
-            Calculator obj1 = new Calculator(c, d); 
-            Calculator obj2 = new Calculator(c, d);
-             Calculator obj3 = new Calculator(c, d);
-              Calculator obj4 = new Calculator(c, d);
+            int a = scan.nextInt(); 
+            int b = scan.nextInt(); 
+            Calculator obj1 = new Calculator(a, b); 
+            Calculator obj2 = new Calculator(a, b); 
+            Calculator obj3 = new Calculator(a, b); 
+            Calculator obj4 = new Calculator(a, b); 
             System.out.println("Addition:" + obj1.Add()); 
             System.out.println("Substraction:" + obj2.Substract()); 
             System.out.println("Multiplication" + obj3.Multiplication()); 
             System.out.println("Division :" + obj4.Division()); 
         } 
         catch(NumberFormatException n){ 
-            System.out.println("write in integer"); 
+            System.out.println("Please write in integer"); 
         } 
  
     } 
